1f03f04b09212d10e7bffc7265f2e27a130b9a4b
rimshakhan359 <getrimshasaeed>
2024-08-13 06:48:16 -0700

    added thymetemplate

---
+++ src/main/java/com/rk/bank/controllers/frontend.java
@@ -0,0 +1,35 @@
+package com.rk.bank.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bank")
+public class frontend {
+
+    @GetMapping("/transactions")
+    public String index(){
+        return "transactions";
+    }
+
+    @GetMapping("/all-clients")
+    public String bankClients(){
+        return "otherUser";
+    }
+
+    @GetMapping("/transaction-history")
+    public String transactionHistory(){
+        return "transferHistory";
+    }
+
+    @GetMapping("/transfer-funds")
+    public String transferFunds(){
+        return "transferMoney";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
