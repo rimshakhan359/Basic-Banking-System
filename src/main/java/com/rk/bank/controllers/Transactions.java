e23555c5f7f493350e448634b359b35817dcf81f
rimshakhan359 <getrimshasaeed>
2024-08-15 19:05:03 -0700

    Rounding mode set to HALF EVEN

--- src/main/java/com/rk/bank/controllers/Transactions.java
+++ src/main/java/com/rk/bank/controllers/Transactions.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -24,14 +25,14 @@ public class Transactions {
     @PutMapping("/transfer-amount")
     public ResponseEntity<Object> transferAmount(@RequestBody HashMap<String,String> request){
 
-       String message = "Amount transfered successfully";
-       HttpStatusCode httpStatusCode = HttpStatus.OK;
+        String message = "Amount transfered successfully";
+        HttpStatusCode httpStatusCode = HttpStatus.OK;
 
-       if(null == request.get("recipientID") || request.get("recipientID").isEmpty()){
-           return ResponseEntity
-                   .status(HttpStatus.BAD_REQUEST)
-                   .body("Valid Recipient ID Required");
-       }
+        if(null == request.get("recipientID") || request.get("recipientID").isEmpty()){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Valid Recipient ID Required");
+        }
 
         if(null == request.get("amount") || request.get("amount").isEmpty()){
             return ResponseEntity
@@ -39,17 +40,18 @@ public class Transactions {
                     .body("Valid Amount Required");
         }
 
-       int recipientID = Integer.parseInt(request.get("recipientID"));
-       BigDecimal bigDecimal = new BigDecimal(request.get("amount"));
+        int recipientID = Integer.parseInt(request.get("recipientID"));
+        BigDecimal amount = new BigDecimal(request.get("amount"));
+        amount = amount.setScale(2, RoundingMode.HALF_EVEN);
 
-       try{
-           transactionsService.transferFundsTrx(recipientID, bigDecimal.doubleValue());
-       }catch (Exception e){
-           e.printStackTrace();
+        try{
+            transactionsService.transferFundsTrx(recipientID, amount.doubleValue());
+        }catch (Exception e){
+            e.printStackTrace();
 
-           message = e.getMessage();
-           httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-       }
+            message = e.getMessage();
+            httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
 
         return ResponseEntity.status(httpStatusCode).body(new RestResponse(message));
     }
